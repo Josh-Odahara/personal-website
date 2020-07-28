3fcd2d9586112bc0d00ca1274904a45f4b9389f9
root <josh.g.odahara@gmail.com>
2020-07-28 08:53:37 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req,res){
+	res.render("home");
+});
+
+app.get("/about", function(req,res){
+	res.render("about");
+});
+
+app.get("/pics", function(req,res){
+	res.render("pics");
+});
+
+app.listen(3000, process.env.IP, function(){
+	console.log("Site is up and running!")
+});
